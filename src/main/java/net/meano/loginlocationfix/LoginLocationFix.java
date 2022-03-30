6c018cd1899ab13030567cd7320dc8daaa7cb832
Dreeam <dreeam_charlie@163.com>
2022-03-30 19:25:01 +0800

    renovate

---
+++ src/main/java/net/meano/loginlocationfix/LoginLocationFix.java
@@ -0,0 +1,21 @@
+package net.meano.loginlocationfix;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LoginLocationFix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //Log开始记录
+        getLogger().info("LoginLocationFix 0.1,by Meano. 正在载入.");
+        PluginManager PM = Bukkit.getServer().getPluginManager();
+        PM.registerEvents(new LoginLocationFixListeners(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
