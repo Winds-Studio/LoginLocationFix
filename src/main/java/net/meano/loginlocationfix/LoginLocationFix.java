7c82114ed9c779b6b76bcf5d1832fd53246d48b3
Dreeam-qwq <61569423+Dreeam-qwq@users.noreply.github.com>
2022-06-23 19:24:38 +0800

    fix

--- src/main/java/net/meano/loginlocationfix/LoginLocationFix.java
+++ src/main/java/net/meano/loginlocationfix/LoginLocationFix.java
@@ -9,6 +9,7 @@ public final class LoginLocationFix extends JavaPlugin {
     @Override
     public void onEnable() {
         //Log开始记录
+        saveDefaultConfig();
         getLogger().info("LoginLocationFix 0.5,by Meano & Dreeam, Loaded.");
         PluginManager PM = Bukkit.getServer().getPluginManager();
         PM.registerEvents(new LoginLocationFixListeners(this), this);
