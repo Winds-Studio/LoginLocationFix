c78e01542a7fc78468a6a9b4a27046550e25a97e
Dreeam-qwq <61569423+Dreeam-qwq@users.noreply.github.com>
2022-06-23 18:46:36 +0800

    format

--- src/main/java/net/meano/loginlocationfix/LoginLocationFixListeners.java
+++ src/main/java/net/meano/loginlocationfix/LoginLocationFixListeners.java
@@ -25,50 +25,52 @@ public class LoginLocationFixListeners implements Listener {
         Material UpType = JoinLocation.getBlock().getRelative(BlockFace.UP).getType();
         if (LLFM.getConfig().getBoolean("Portal.Enable")) {
             if (JoinLocation.getBlock().getType().equals(Material.NETHER_PORTAL) || JoinLocation.getBlock().getRelative(BlockFace.UP).getType().equals(Material.NETHER_PORTAL)) {
-            Block JoinBlock = JoinLocation.getBlock();
-            if (JoinBlock.getRelative(BlockFace.WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.WEST).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.EAST).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.NORTH).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.SOUTH).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.SOUTH_EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH_EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH_EAST).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.SOUTH_WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH_WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH_WEST).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.NORTH_EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH_EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH_EAST).getLocation().add(0.5, 0.1, 0.5));
-            } else if (JoinBlock.getRelative(BlockFace.NORTH_WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH_WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH_WEST).getLocation().add(0.5, 0.1, 0.5));
-            } else {
-                JoinBlock.getRelative(BlockFace.UP).breakNaturally();
-                JoinBlock.breakNaturally();
-            }
-            JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Portal.Message")));
-
-        }} else if (LLFM.getConfig().getBoolean("Underground.Enable")) {
-            if (UpType.isOccluding() || UpType.equals(Material.LAVA)){
-            int MaxHeight = JoinPlayer.getWorld().getMaxHeight() - 2;
-            for (int i = 60; i <= MaxHeight; i++) {
-                JoinLocation.setY(i);
                 Block JoinBlock = JoinLocation.getBlock();
-                if ((JoinBlock.getRelative(BlockFace.DOWN).getType().isBlock())
-                        && JoinBlock.getType().equals(Material.AIR)
-                        && JoinBlock.getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                    if (JoinBlock.getRelative(BlockFace.DOWN).getType().equals(Material.LAVA)) {
-                        JoinBlock.getRelative(BlockFace.DOWN).setType(Material.DIRT);
-                    }
-                    JoinPlayer.teleport(JoinBlock.getLocation().add(0.5, 0.1, 0.5));
-                    JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Underground.Message1")));
-                    break;
+                if (JoinBlock.getRelative(BlockFace.WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.WEST).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.EAST).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.NORTH).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.SOUTH).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.SOUTH_EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH_EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH_EAST).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.SOUTH_WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.SOUTH_WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.SOUTH_WEST).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.NORTH_EAST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH_EAST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH_EAST).getLocation().add(0.5, 0.1, 0.5));
+                } else if (JoinBlock.getRelative(BlockFace.NORTH_WEST).getType().equals(Material.AIR) && JoinBlock.getRelative(BlockFace.NORTH_WEST).getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                    JoinPlayer.teleport(JoinBlock.getRelative(BlockFace.NORTH_WEST).getLocation().add(0.5, 0.1, 0.5));
+                } else {
+                    JoinBlock.getRelative(BlockFace.UP).breakNaturally();
+                    JoinBlock.breakNaturally();
                 }
-                if (i == MaxHeight) {
-                    JoinPlayer.teleport(JoinBlock.getLocation().add(0.5, 1.1, 0.5));
-                    JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Underground.Message2")));
+                JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Portal.Message")));
+
+            }
+        } else if (LLFM.getConfig().getBoolean("Underground.Enable")) {
+            if (UpType.isOccluding() || UpType.equals(Material.LAVA)) {
+                int MaxHeight = JoinPlayer.getWorld().getMaxHeight() - 2;
+                for (int i = 60; i <= MaxHeight; i++) {
+                    JoinLocation.setY(i);
+                    Block JoinBlock = JoinLocation.getBlock();
+                    if ((JoinBlock.getRelative(BlockFace.DOWN).getType().isBlock())
+                            && JoinBlock.getType().equals(Material.AIR)
+                            && JoinBlock.getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                        if (JoinBlock.getRelative(BlockFace.DOWN).getType().equals(Material.LAVA)) {
+                            JoinBlock.getRelative(BlockFace.DOWN).setType(Material.DIRT);
+                        }
+                        JoinPlayer.teleport(JoinBlock.getLocation().add(0.5, 0.1, 0.5));
+                        JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Underground.Message1")));
+                        break;
+                    }
+                    if (i == MaxHeight) {
+                        JoinPlayer.teleport(JoinBlock.getLocation().add(0.5, 1.1, 0.5));
+                        JoinPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', LLFM.getConfig().getString("Underground.Message2")));
+                    }
                 }
             }
-        } }
+        }
     }
 }
