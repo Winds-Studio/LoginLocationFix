0f1e40c3b7ab877cc57c72a6080855561e303ffc
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-04-25 21:38:31 -0400

    Bump
    * Bump dependencies
    * Add TODO

--- README.md
+++ README.md
@@ -9,9 +9,13 @@ Fix location of players when they join the server.
 Compatibility
 
 - Support Java 8 and higher
-- Support 1.8.x - Latest Minecraft version (1.20.4)
+- Support 1.8.x - Latest Minecraft version (1.20.5)
 - Compatible with Spigot / Paper / Paper Forks / Forge+Bukkit Hybrid Server
 - Folia Support
+
+## TODO
+
+- Move compatibility code to util class
 
 # Contact
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     `java-library`
-    id("io.github.goooler.shadow") version "8.1.7" apply true
+    id("io.github.goooler.shadow") version "8.1.7"
 }
 
 group = "net.meano"
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenCentral()
@@ -31,7 +31,7 @@ repositories {
 val adventureVersion = "4.16.0"
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.5-R0.1-SNAPSHOT")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
     api("com.tcoded:FoliaLib:0.3.1")
